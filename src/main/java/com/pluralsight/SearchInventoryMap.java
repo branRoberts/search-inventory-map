183e78feae97d8e3c75591cb2bba304d4c28a2d9
branRoberts <brandonkrob12@gmail.com>
2026-04-22 14:43:46 -0400

    Second Commit Quickly added some code to read off the .csv file and store it into variables

--- src/main/java/com/pluralsight/SearchInventoryMap.java
+++ src/main/java/com/pluralsight/SearchInventoryMap.java
@@ -1,4 +1,47 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class SearchInventoryMap {
+
+    public static HashMap<Integer, Product> inventory =
+            new HashMap<Integer, Product>();
+
+    public static void main(String[] args) {
+        // this method loads product objects into inventory
+        loadInventory();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What item # are you interested in? ");
+        int id = scanner.nextInt();
+        Product matchedProduct = inventory.get(id);
+        if (matchedProduct == null) {
+            System.out.println("We don't carry that product");
+        } else {
+            System.out.printf("We carry %s and the price is $%.2f",
+                    matchedProduct.getName(), matchedProduct.getPrice());
+        }
+    }
+
+    private static void loadInventory(){
+        // Your code should be here
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("inventory.csv"));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split("\\|");
+                int id = Integer.parseInt(values[0]);
+                String name = values[1];
+                double price = Double.parseDouble(values[2]);
+            }
+            reader.close();
+        }catch (Exception e){
+            System.err.println("Error loading Inventory");
+        }
+    }
+
 }
